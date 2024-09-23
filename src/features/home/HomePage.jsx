7d013512eb619fa949d4840b0008b5ddb8e881e5
mdnob <mdnobz3@gmail.com>
2024-09-22 20:42:47 -0400

    Add home page buttons.

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,18 +1,26 @@
-import { Button, Card, Container, Col, Row } from 'react-bootstrap';
+import { Button, Card, Container, Col, Row, ListGroup } from 'react-bootstrap';
 
 function HomePage() {
   return (
-    <Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-      <Row> 
-        <Col> 
-          <h1>Trivia</h1>
-          <Card>
-            <Button>Play Game</Button>
+    <Container fluid className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+      <Row className="justify-content-center">
+        <Col md={12}>
+          <Card className="text-center">
+            <Card.Body>
+              <Card.Title>Trivia AI Adventure</Card.Title>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Button href='#room'>Create Room</Button>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button href='#join'>Join Room</Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
     </Container>
-
   );
 }
 
