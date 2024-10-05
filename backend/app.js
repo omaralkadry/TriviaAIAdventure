129842ed07f98e90e705e65b7e26309a4b3bb452
paulj003 <pauljablonowski@ufl.edu>
2024-10-05 16:12:39 -0400

    Added cors and edited references

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
+// Followed tutorial from https://www.mongodb.com/resources/languages/mern-stack-tutorial
 // Some code from https://expressjs.com/en/starter/hello-world.html
-// Some code from https://www.mongodb.com/resources/languages/mern-stack-tutorial
 // Referenced https://masteringjs.io/tutorials/express/post
+// Referenced https://github.com/expressjs/cors?tab=readme-ov-file
+// Asked ChatGPT about how to use CORS
 const express = require('express');
+const cors = require('cors');
 const register = require('./routes/register.js');
 const login = require('./routes/login.js');
+
+const corsOptions = {
+    origin: '*',
+    // origin: 'http://google.com', // Front-end URL maybe
+    optionsSuccessStatus: 200 // Used for some legacy browsers
+}
 
 const app = express();
 const port = 3000;
 
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/register", register);
 app.use("/login", login);
