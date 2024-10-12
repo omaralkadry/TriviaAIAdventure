fac9e663de50e2507827c0662a41a8e2e19779be
paulj003 <pauljablonowski@ufl.edu>
2024-10-12 01:14:19 -0400

    Added create room (works) and generate questions (might not work) events

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@
 // Some code from https://expressjs.com/en/starter/hello-world.html
 // Referenced https://masteringjs.io/tutorials/express/post
 // Referenced https://github.com/expressjs/cors?tab=readme-ov-file
-// Referenced https://socket.io/docs/v4/tutorial/introduction
-// Used ChatGPT (asked about CORS, client-client and client-server communication, etc.)
+// Referenced tutorial and documentation from https://socket.io/
+// Used ChatGPT (asked about CORS, client-client and client-server communication, and more)
 const express = require('express');
 const cors = require('cors');
 const register = require('./routes/register.js');
@@ -11,6 +11,7 @@ const login = require('./routes/login.js');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 const { join } = require('node:path');
+const { ClassicTrivia } = require('./gamemodes.js');
 
 const corsOptions = {
     origin: '*',
@@ -31,8 +32,8 @@ app.use(express.json());
 app.use("/register", register);
 app.use("/login", login);
 
-// Used for testing
-/*app.get('/', (req, res) => {
+// Some code below is used for testing
+app.get('/', (req, res) => {
     res.send("Test");
 });
 
@@ -48,11 +49,48 @@ socketIO.on('connection', (socket) => {
         console.log('Message: ' + msg);
     });
 
+    // Called when frontend sends event to create a room
+    socket.on('create room', (callback) => {
+        // Used ChatGPT to see how to generate a random number in a range
+        largestRoomNumber = 90000;
+        smallestRoomNumber = 10000; 
+        const roomCode = Math.floor(Math.random() * (largestRoomNumber - smallestRoomNumber + 1)) + smallestRoomNumber;
+
+        // Joins the room
+        socket.join(roomCode);
+
+        // socketIO.to(roomCode).emit('test', "Message sent in a room");
+
+        callback({
+            // The room code is returned to the frontend
+            roomCode: roomCode
+        });
+    });
+
+    // Might not work correctly
+    socket.on('generate questions', (callback) => {
+        const classicTrivia = new ClassicTrivia();
+
+        // Getting errors when this runs:
+        // SyntaxError: Unexpected token S in JSON at position 0
+        // SyntaxError: Unexpected token F in JSON at position 0
+        questionsOutput = classicTrivia.generateQuestion();
+
+        callback({
+            // This currently returns all the parsed questions
+            // We can alter this event or make a new one so it returns one question at a time
+            questions: questionsOutput
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('A user disconnected')
     });
 });
-*/
+
+socketIO.on('connection', (socket) => {
+
+});
 
 server.listen(port, () => {
     console.log(`App available at http://localhost:${port}`);
