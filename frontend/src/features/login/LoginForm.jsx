709d03c1c235d91aa4154766cad5a5ca8bfe36e1
paulj003 <pauljablonowski@ufl.edu>
2024-10-17 01:07:06 -0400

    Added fetch code for logging in

--- frontend/src/features/login/LoginForm.jsx
+++ frontend/src/features/login/LoginForm.jsx
@@ -1,16 +1,42 @@
-
+// Referenced https://www.geeksforgeeks.org/javascript-fetch-method/
+// Referenced https://www.w3schools.com/html/html_form_input_types.asp
 import { useState } from 'react';
 import { Button, Card, Container, Col, Form, Row } from 'react-bootstrap'
 
 function LoginForm({ onLogin }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // const [loginSuccessful, setLoginSuccessful] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Handle login logic here
-    onLogin({ email, password });
+    // onLogin({ email, password });
+
+    // Referenced https://www.geeksforgeeks.org/javascript-fetch-method/
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username: email, password: password})
+    };
+
+    // Status Code 200 (Status Ok) means login was successful
+    fetch("http://localhost:3000/login", options)
+      .then(response => {
+        if (response.ok) {
+          // Login was successful
+          // setLoginSuccessful(true);
+          console.log("Login Successful");
+        }
+        else {
+          // Login was not successful
+          // setLoginSuccessful(false);
+          console.log("Login Unsuccessful");
+        }
+      })
   };
 
   return (
@@ -28,11 +54,11 @@ function LoginForm({ onLogin }) {
             <Form onSubmit={handleSubmit}> 
               <Form.Group 
                 className="mb-3" 
-                controlId="formBasicEmail"
+                controlId="formBasicUsername"
               >
                 <Form.Label>Email</Form.Label>
                 <Form.Control 
-                  type="email" 
+                  type="text" 
                   placeholder="Email" 
                   value={email} 
                   onChange={(e) => setEmail(e.target.value)} 
