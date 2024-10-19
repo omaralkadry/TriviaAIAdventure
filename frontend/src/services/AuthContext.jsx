a5ed08a42dd48e52c27d47c9a84fc49a155a1cf6
mdnob <mdnobz3@gmail.com>
2024-10-18 23:25:57 -0400

    Update authentication to use sessionStorage

--- frontend/src/services/AuthContext.jsx
+++ frontend/src/services/AuthContext.jsx
@@ -5,18 +5,47 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  // Save user data in state
+  // Save user data in session storage
   const login = (userData) => {
+    // Converts userData javascript object to JSON string
+    // Then stores into sessionStorage
+    sessionStorage.setItem('user', JSON.stringify(userData));
     setUser(userData);
   };
   
-  // Clear user data on logout
+  // Clear user data in session storage on logout
   const logout = () => {
+    sessionStorage.removeItem('user');
     setUser(null);
   };
 
+  // Returns original javascript object by parsing
+  const getUser = () => {
+    const storedUser = sessionStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  };
+
+  const isAuthenticated = () => {
+    return getUser() !== null;
+  };
+
+  const getID = () => {
+    setUser(getUser());
+    return user._id;
+  }
+
+  const getUsername = () => {
+    setUser(getUser());
+    return user.username;
+  }
+
+  const getPassword = () => {
+    setUser(getUser());
+    return user.getPassword;
+  }
+
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, logout, isAuthenticated, getID, getUsername, getPassword }}>
       {children}
     </AuthContext.Provider>
   );
